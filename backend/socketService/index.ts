8a4e648b567a2df693af37675fd9f936b91d97ff
M_NAMEER_K <nameerkhan0123@gmail.com>
2026-03-12 16:45:36 +0530

    install axios

--- backend/socketService/index.ts
+++ backend/socketService/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["chatter-seven-blue.vercel.app","http://localhost:5173"],
   credentials: true
 }));
 
--- backend/userService/index.ts
+++ backend/userService/index.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[ "chatter-seven-blue.vercel.app","http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],

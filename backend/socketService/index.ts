a070a42556ed4d6f29f453dd67bc0a2fda691183
M_NAMEER_K <nameerkhan0123@gmail.com>
2026-03-13 12:38:55 +0530

    install axios

--- backend/socketService/index.ts
+++ backend/socketService/index.ts
@@ -30,7 +30,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true
   }
 });

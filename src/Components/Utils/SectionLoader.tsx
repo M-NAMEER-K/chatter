5408f8c94f96a126682ea00845f10946f62ce726
M-NAMEER-K <116507348+M-NAMEER-K@users.noreply.github.com>
2026-03-19 21:14:15 +0530

    Create SectionLoader.tsx

---
+++ src/Components/Utils/SectionLoader.tsx
@@ -0,0 +1,7 @@
+const SectionLoader = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="h-10 w-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+};
